4ec3a7d2f0cb99e52b1fe1b752b14c616e415351
Tony Eriksson <ab5812@student.jamk.fi>
2022-03-09 14:36:02 +0200

    player movement

--- Team_Rats_CONBIT_JAM/Assets/Scripts/Player/PlayerMovement.cs
+++ Team_Rats_CONBIT_JAM/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float xMove;
+    [SerializeField] float yMove;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        yMove = Input.GetAxis("Vertical");
+        xMove = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(xMove, yMove);
+
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.deltaTime);
     }
 }

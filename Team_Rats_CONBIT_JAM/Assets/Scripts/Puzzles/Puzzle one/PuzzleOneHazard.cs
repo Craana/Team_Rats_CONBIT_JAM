256ba70f434883bd9c589d3c1d8d0cf3520e3fe2
superduperjones <74876897+superduperjones@users.noreply.github.com>
2022-03-10 16:34:37 +0200

    puzzle1

--- Team_Rats_CONBIT_JAM/Assets/Scripts/Puzzles/Puzzle one/PuzzleOneHazard.cs
+++ Team_Rats_CONBIT_JAM/Assets/Scripts/Puzzles/Puzzle one/PuzzleOneHazard.cs
@@ -8,13 +8,16 @@ public class PuzzleOneHazard : MonoBehaviour
 
     [SerializeField] float restartTime;
     [SerializeField] GameObject Player;
+    public AudioSource source;
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(collision.gameObject);
-            StartCoroutine(WaitLoadScene());
+            source.Play();
+            Destroy(collision.gameObject);           
+            StartCoroutine(WaitLoadScene());        
         }
     }
 
